d5d44a0e4c3fb4d4fd224474895fe5d7b78ba5be
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-10 04:43:52 -0400

    implemented fav badge alert

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,12 +3,13 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-export const FavBadge = ({ isFavPhotoExist }) => {
+export const FavBadge = ({ likedPhotoArray }) => {
+  const displayAlert = likedPhotoArray.length > 0;
   return (
     <div className='fav-badge'>
-      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist}/>
+      <FavIcon width={20} height={17} fill="#C80000" displayAlert={displayAlert} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
