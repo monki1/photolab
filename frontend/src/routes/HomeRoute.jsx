76a11b797310d9b0ebcc4a3b49b6829dac707c36
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-10 03:47:26 -0400

    home route

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => 
-  <div className="home-route">
-    {/* Insert React */}
-  </div>
+const HomeRoute = (props) => {
+
+  const { topicData, likedPhotoArray, onLoadTopic, photoData, onClickLikes, onClickModal } = props;
+
+  return (
+    <div className='home-route'>
+      <TopNavigationBar topicData={topicData}
+        likedPhotoArray={likedPhotoArray}
+        onLoadTopic={onLoadTopic} />
+
+      <PhotoList photoData={photoData}
+        onClickLikes={onClickLikes}
+        onClickModal={onClickModal}
+        likedPhotoArray={likedPhotoArray}
+        onLoadTopic={onLoadTopic} />
+    </div>
+  );
+};
 
 export default HomeRoute;
