7de43edef1a798b758fe948a6d211e0b9d2925de
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-10 03:56:25 -0400

    added topic list

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 
+import TopicListItem from './TopicListItem';
 import '../styles/TopicList.scss';
 
-const TopicList = () => {
-  <div className="top-nav-bar__topic-list">
-    {/* Insert React */}
-  </div>
-}
+const TopicList = (props) => {
+  const { onLoadTopic, topicData } = props;
 
-TopicList.defaultProps = {
-  topics: [
-    {
-      "id": "1",
-      "slug": "topic-1",
-      "title": "Nature"
-    },  
-    {
-      "id": "2",
-      "slug": "topic-2",
-      "title": "Travel"
-    },
-    {
-      "id": "3",
-      "slug": "topic-3",
-      "title": "People"
-    },
-  ]
-}
-export default TopicList
+  const topics = topicData.map((topic) => {
+    return <TopicListItem key={topic.id} {...topic} onLoadTopic={onLoadTopic} />;
+  });
+
+  return (
+    <div className="top-nav-bar--topic-list">
+      {topics}
+    </div>
+  );
+
+};
+
+export default TopicList;
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import '../styles/TopicListItem'
+import '../styles/TopicListItem.scss';
 
-const TopicListItem = () => {
-  <div className="topic-list__item">
-    {/* Insert React */}
-  </div>
-}
+const TopicListItem = (props) => {
+  const { onLoadTopic, title, id } = props;
 
-TopicListItem.defaultProps =   {
-  "id": "1",
-  "slug": "topic-1",
-  "label": "Nature"
-}
-export default TopicListItem
+  return (
+    <div className="topic-list--item" onClick={() => { onLoadTopic(id); }}>
+      <span>
+        {title}
+      </span>
+    </div>
+  );
+
+};
+
+export default TopicListItem;
