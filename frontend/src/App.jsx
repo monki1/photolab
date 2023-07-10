f51536406d1f7b6a701688cb841a71d06b1995da
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-10 03:48:45 -0400

    app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import useApplicationData from './hooks/useApplicationData';
 
-import PhotoListItem from './components/PhotoListItem';
+
 import './App.scss';
+import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 
-// Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+//state logic from useApplicationData
+const App = () => {
+  
+  console.log("running");
+  const { photoData,
+    topicData,
+    likedPhotoArray,
+    displayModal,
+    modalData,
+    onClickLikes,
+    onClickModal,
+    onClose,
+    onLoadTopic } = useApplicationData();
 
-export default App
+    console.log(photoData);
+  //rendering Homeroute and Modal route
+  return (
+    < div className="App" >
+      <HomeRoute
+        topicData={topicData}
+        photoData={photoData}
+        onClickModal={onClickModal}
+        onClickLikes={onClickLikes}
+        likedPhotoArray={likedPhotoArray}
+        onLoadTopic={onLoadTopic} />
+      {displayModal &&
+        (<PhotoDetailsModal
+          photoData={photoData}
+          onClose={onClose}
+          modalData={modalData}
+          onClickModal={onClickModal}
+          onClickLikes={onClickLikes}
+          likedPhotoArray={likedPhotoArray}
+        />
+        )}
+    </div >
+  );
+};
+
+export default App;
+
+
